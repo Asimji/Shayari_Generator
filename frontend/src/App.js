26126e680497f479595d5d2ce20c1a9f7a6d1f2f
Asim <mohdasim.srk@gmailcom>
2023-10-09 17:05:01 +0530

    dynamic

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,12 @@
 import {useState} from "react"
 import './App.css';
+import { Box, Button, Center, Flex, Heading, Input } from "@chakra-ui/react";
 
 function App() {
 
   const [res,setRes]=useState("")
   const [value,setValue]=useState("")
   
- 
-
   const handleSubmit=()=>{
  fetch('https://shayari-t389.onrender.com/shayari',{
     method:"POST",
@@ -19,14 +18,35 @@ function App() {
   }
 
   return (
-    <div >
-      <div >
- <h1>Shayari Convertor</h1>
- <div style={{display:'flex'}}>
- <input placeholder="Type any word" onChange={(e)=>setValue(e.target.value)}/>
- <button onClick={handleSubmit}>Submit</button>
- </div>
- <div style={{border:"2px solid gray",height:'50vh',marginTop:'2vh'}}>{res} </div>
+    <div style={{position:'relative',height:'97vh',width:'99vw',}}>
+      <div style={{position:'absolute',height:'50vh',width:'50vw',top:'20%',left:'25%',transform:'translate(-50% -50%)'}}   >
+      <Center>
+        <Heading 
+        bgGradient="linear(to-r, #ef5350, #f48fb1, #7e57c2, #2196f3, #26c6da, #43a047, #eeff41, #f9a825, #ff5722)"
+        backgroundClip="text"
+        color="transparent"
+        as="span"
+        >
+        Shayari Generator
+        </Heading>
+        </Center>
+        <Flex gap={'5vh'} p={'5vh'} w={'100%'} >
+<Input placeholder="Type Word for Shayri"  onChange={(e)=>setValue(e.target.value)}/>
+
+<Button onClick={handleSubmit}>Generate</Button>
+</Flex>
+<Box border={'1px solid #F012BE'} w={'100%'} h='40vh' borderRadius={'2vh'} color='#7FDBFF' p={'5vh'}
+fontFamily={'sans-serif'} 
+ fontSize={'1.5rem'}
+>{!res ? (
+  !value ? (
+    <Heading>Welcome to our Shayari generator! Enter a word and click 'Generate' to create your Shayari.</Heading>
+  ) : (
+    <Heading>Your shayari will be available shortly!</Heading>
+  )
+) : (
+  res
+)}</Box>
       </div>
     </div>
   );
